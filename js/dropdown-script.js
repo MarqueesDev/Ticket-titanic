19cad1f0af203e29a405bb504dbdb3211aad5ce7
Alan Marques de Aguiar (M0rdek4y) <116358048+MarquesDesigner@users.noreply.github.com>
2024-06-05 02:05:23 -0300

    Add resposive navigation menu

---
+++ js/dropdown-script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.querySelector('.menu-icon');
+    const dropdown = document.querySelector('.dropdown');
+    const menuIconOpen = document.querySelector('.menu-icon-open');
+    const menuIconClose = document.querySelector('.menu-icon-close');
+
+    menuIcon.addEventListener('click', () => {
+        dropdown.classList.toggle('show-dropdown');
+        menuIconOpen.style.display = dropdown.classList.contains('show-dropdown') ? 'none' : 'block';
+        menuIconClose.style.display = dropdown.classList.contains('show-dropdown') ? 'block' : 'none';
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.menu-icon') && !e.target.closest('.dropdown')) {
+            dropdown.classList.remove('show-dropdown');
+            menuIconOpen.style.display = 'block';
+            menuIconClose.style.display = 'none';
+        }
+    });
+});
